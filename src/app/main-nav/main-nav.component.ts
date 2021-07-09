a0567bf957b31f670af262872d37c5bb6ed65d66
shaque525 <40909491+shaque525@users.noreply.github.com>
2021-07-09 17:18:31 +0530

    Add files via upload

---
+++ src/app/main-nav/main-nav.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-main-nav',
+  templateUrl: './main-nav.component.html',
+  styleUrls: ['./main-nav.component.scss']
+})
+export class MainNavComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver, private route:Router) {}
+
+  onCheckout() {
+      this.route.navigate(['/checkout']);
+  }
+}
